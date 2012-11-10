487ea8c8b3ee982076005d0650be3479927604e6
VampireTeeth <vampireteeth001@gmail.com>
2012-11-11 00:56:21 +1100

    lastModified property added to Category

--- JPA-study/src/main/java/com/steventk/jpastudy/Category.java
+++ JPA-study/src/main/java/com/steventk/jpastudy/Category.java
@@ -1,27 +1,49 @@
 package com.steventk.jpastudy;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="CATEGORY")
+@Table(name="`CATEGORY`")
 public class Category {
     private Long id;
+    private Date lastModified;
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="CATEGORY_ID")
+    @Column(name="`CATEGORY_ID`")
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    void setId(Long id) {
         this.id = id;
+    }
+
+    @Column(name="`LAST_MODIFIED`")
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
     }
     
     
+    @PrePersist
+    @PreUpdate
+    void updateTimestamps() {
+        this.lastModified = new Date();
+    }
 }
--- JPA-study/src/test/java/com/steventk/jpastudy/EntityManagerTest.java
+++ JPA-study/src/test/java/com/steventk/jpastudy/EntityManagerTest.java
@@ -7,28 +7,38 @@ import javax.persistence.Persistence;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class EntityManagerTest {
 
     private static final String EMF_NAME = "mydb-manager";
+    
+    private EntityManagerFactory emf;
+    private EntityManager em;
+    
+    @Before
+    public void setUp() throws Exception {
+        this.emf = Persistence.createEntityManagerFactory(EMF_NAME);
+        this.em = this.emf.createEntityManager();
+    }
     @Test
     public void testInstantiationOfEntityManager() throws Exception {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory(EMF_NAME);
         Assert.assertNotNull(emf);
     }
     
     @Test
     public void testCategoryEntity() throws Exception {
         Category category = new Category();
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory(EMF_NAME);
         Assert.assertNotNull(emf);
-        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         em.persist(category);
         tx.commit();
         em.close();
+        Assert.assertNotNull(category.getId());
         Assert.assertEquals(new Long(1), category.getId());
+        Assert.assertNotNull(category.getLastModified());
     }
+    
 }
